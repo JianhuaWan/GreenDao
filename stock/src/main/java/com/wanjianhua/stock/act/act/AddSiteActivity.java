07001a9ac9ee8a9556eaf53d79ada5faa2202544
wanjianhua <1243381493@qq.com>
2017-04-05 17:46:38 +0800

    和仔工作室2017年4月5日 17:46:24

--- stock/src/main/java/com/wanjianhua/stock/act/act/AddSiteActivity.java
+++ stock/src/main/java/com/wanjianhua/stock/act/act/AddSiteActivity.java
@@ -2,6 +2,10 @@ package com.wanjianhua.stock.act.act;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.wanjianhua.stock.R;
 import com.wanjianhua.stock.act.base.BaseActivity;
@@ -11,9 +15,32 @@ import com.wanjianhua.stock.act.base.BaseActivity;
  */
 
 public class AddSiteActivity extends BaseActivity {
+    private View childView;
+    private LinearLayout linear_detail;
+    private TextView tv_level;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addsite_main);
+        initView();
+
+    }
+
+    private void initView() {
+        linear_detail = (LinearLayout) findViewById(R.id.linear_detail);
+        for (int i = 0; i < 3; i++) {
+            childView = LayoutInflater.from(AddSiteActivity.this).inflate(
+                    R.layout.detail_child, null);
+            tv_level = (TextView) childView.findViewById(R.id.tv_level);
+            if (i == 0) {
+                tv_level.setText(getString(R.string.level1));
+            } else if (i == 1) {
+                tv_level.setText(getString(R.string.level2));
+            } else if (i == 2) {
+                tv_level.setText(getString(R.string.level3));
+            }
+            linear_detail.addView(childView);
+        }
     }
 }
--- stock/src/main/java/com/wanjianhua/stock/act/act/SiteDetailActivity.java
+++ stock/src/main/java/com/wanjianhua/stock/act/act/SiteDetailActivity.java
@@ -2,6 +2,10 @@ package com.wanjianhua.stock.act.act;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.wanjianhua.stock.R;
 import com.wanjianhua.stock.act.base.BaseActivity;
@@ -11,9 +15,32 @@ import com.wanjianhua.stock.act.base.BaseActivity;
  */
 
 public class SiteDetailActivity extends BaseActivity {
+    private LinearLayout linear_detail;
+    private View childview;
+    private TextView tv_level;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.site_detail);
+        initView();
+    }
+
+
+    private void initView() {
+        linear_detail = (LinearLayout) findViewById(R.id.linear_detail);
+        for (int i = 0; i < 3; i++) {
+            childview = LayoutInflater.from(SiteDetailActivity.this).inflate(
+                    R.layout.detail_child, null);
+            tv_level = (TextView) childview.findViewById(R.id.tv_level);
+            if (i == 0) {
+                tv_level.setText(getString(R.string.level1));
+            } else if (i == 1) {
+                tv_level.setText(getString(R.string.level2));
+            } else if (i == 2) {
+                tv_level.setText(getString(R.string.level3));
+            }
+            linear_detail.addView(childview);
+        }
     }
 }
