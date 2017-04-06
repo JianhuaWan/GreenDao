1d2bbfc20e5261239b51bf4ce69f06e762a6a75d
wanjianhua <1243381493@qq.com>
2017-04-06 11:49:39 +0800

    更改界面(后续改ui,今天处理逻辑显示效果)

--- stock/src/main/java/com/wanjianhua/stock/act/act/AddSiteActivity.java
+++ stock/src/main/java/com/wanjianhua/stock/act/act/AddSiteActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,10 +15,12 @@ import com.wanjianhua.stock.act.base.BaseActivity;
  * Created by wanjianhua on 2017/4/5.
  */
 
-public class AddSiteActivity extends BaseActivity {
+public class AddSiteActivity extends BaseActivity implements View.OnClickListener {
     private View childView;
     private LinearLayout linear_detail;
-    private TextView tv_level;
+    private TextView tv_waveband, tv_levelchild;
+    private ImageView img_back;
+    private TextView tv_add;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,19 +31,47 @@ public class AddSiteActivity extends BaseActivity {
     }
 
     private void initView() {
+        img_back = (ImageView) findViewById(R.id.img_back);
+        img_back.setOnClickListener(this);
+        tv_add = (TextView) findViewById(R.id.tv_add);
+        tv_add.setOnClickListener(this);
         linear_detail = (LinearLayout) findViewById(R.id.linear_detail);
         for (int i = 0; i < 3; i++) {
-            childView = LayoutInflater.from(AddSiteActivity.this).inflate(
-                    R.layout.detail_child, null);
-            tv_level = (TextView) childView.findViewById(R.id.tv_level);
-            if (i == 0) {
-                tv_level.setText(getString(R.string.level1));
-            } else if (i == 1) {
-                tv_level.setText(getString(R.string.level2));
-            } else if (i == 2) {
-                tv_level.setText(getString(R.string.level3));
+            for (int j = 0; j < 3; j++) {
+                childView = LayoutInflater.from(AddSiteActivity.this).inflate(
+                        R.layout.detail_child, null);
+                tv_levelchild = (TextView) childView.findViewById(R.id.tv_level);
+                tv_waveband = (TextView) childView.findViewById(R.id.tv_waveband);
+                linear_detail.addView(childView);
+                if (j == 0) {
+                    tv_waveband.setText(getString(R.string.wareband1));
+                } else if (j == 1) {
+                    tv_waveband.setText(getString(R.string.wareband2));
+                    tv_levelchild.setVisibility(View.GONE);
+                } else if (j == 2) {
+                    tv_waveband.setText(getString(R.string.wareband3));
+                    tv_levelchild.setVisibility(View.GONE);
+                }
+                if (i == 0) {
+                    tv_levelchild.setText(getString(R.string.level1));
+                } else if (i == 1) {
+                    tv_levelchild.setText(getString(R.string.level2));
+                } else if (i == 2) {
+                    tv_levelchild.setText(getString(R.string.level3));
+                }
             }
-            linear_detail.addView(childView);
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.img_back:
+                finish();
+                break;
+            case R.id.tv_add:
+                break;
         }
     }
 }
--- stock/src/main/java/com/wanjianhua/stock/act/act/SiteDetailActivity.java
+++ stock/src/main/java/com/wanjianhua/stock/act/act/SiteDetailActivity.java
@@ -16,8 +16,8 @@ import com.wanjianhua.stock.act.base.BaseActivity;
 
 public class SiteDetailActivity extends BaseActivity {
     private LinearLayout linear_detail;
-    private View childview;
-    private TextView tv_level;
+    private View childView;
+    private TextView tv_levelchild, tv_waveband;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,17 +30,30 @@ public class SiteDetailActivity extends BaseActivity {
     private void initView() {
         linear_detail = (LinearLayout) findViewById(R.id.linear_detail);
         for (int i = 0; i < 3; i++) {
-            childview = LayoutInflater.from(SiteDetailActivity.this).inflate(
-                    R.layout.detail_child, null);
-            tv_level = (TextView) childview.findViewById(R.id.tv_level);
-            if (i == 0) {
-                tv_level.setText(getString(R.string.level1));
-            } else if (i == 1) {
-                tv_level.setText(getString(R.string.level2));
-            } else if (i == 2) {
-                tv_level.setText(getString(R.string.level3));
+            for (int j = 0; j < 3; j++) {
+                childView = LayoutInflater.from(SiteDetailActivity.this).inflate(
+                        R.layout.detail_child, null);
+                tv_levelchild = (TextView) childView.findViewById(R.id.tv_level);
+                tv_waveband = (TextView) childView.findViewById(R.id.tv_waveband);
+                linear_detail.addView(childView);
+                if (j == 0) {
+                    tv_waveband.setText(getString(R.string.wareband1));
+                } else if (j == 1) {
+                    tv_waveband.setText(getString(R.string.wareband2));
+                    tv_levelchild.setVisibility(View.GONE);
+                } else if (j == 2) {
+                    tv_waveband.setText(getString(R.string.wareband3));
+                    tv_levelchild.setVisibility(View.GONE);
+                }
+                if (i == 0) {
+                    tv_levelchild.setText(getString(R.string.level1));
+                } else if (i == 1) {
+                    tv_levelchild.setText(getString(R.string.level2));
+                } else if (i == 2) {
+                    tv_levelchild.setText(getString(R.string.level3));
+                }
             }
-            linear_detail.addView(childview);
+
         }
     }
 }
